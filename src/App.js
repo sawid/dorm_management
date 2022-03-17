65a616c251b7bdbc56400e0af132c076b9a2f5fd
Sawid Limkiatsataporn <zasawidza@gmail.com>
2022-03-17 11:45:16 +0700

    update 0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPage from "./components/main-page.component";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <nav className="navbar navbar-dark bg-dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/"} className="nav-link">
+                  React Dorm System
+                </Link>
+              </Navbar.Brand>
+
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/"} className="nav-link">
+                    main
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </nav>
+        <Container>
+          <Row>
+            <Col md="12">
+              <div className="wrapper">
+                <Routes>
+                  <Route exact path="/" element={<MainPage />} />
+                </Routes>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/main-page.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Mainpage extends Component {
+  render() {
+    return (
+      <p>MainPage</p>
+    )
+  }
+}

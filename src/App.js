8ad2513efcb79021fb37019f9370ae91145dbf2e
Sawid Limkiatsataporn <zasawidza@gmail.com>
2022-04-07 14:49:10 +0700

    Update Routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
+import React from "react";
 import MainPage from "./components/main-page.component";
 import Menu from "./components/sidebar.component";
 import Header from "./components/header.component";
 import Footer from "./components/footer.component";
 import Dashboard from "./components/dashboard.component";
-
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Loginpage from "./components/login.component";
+import Registerpage from "./components/register.component";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Withnav from "./components/withnav.component";
-import Loginpage from "./components/login.component";
-import Withoutnav from "./components/withoutnav.component";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
     <div class="wrapper">
       <Router>
-        <Header />
-        <Menu />
         <Routes>
-          <Route path="/" exact element={<Dashboard />}/>
-          <Route path="/login" element={<Loginpage />}/>
+          <Route element={<WithoutNav />}>
+            <Route path="/login" element={<Loginpage />} />
+            <Route path="/register" element={<Registerpage />} />
+          </Route>
+          <Route element={<WithNav />}>
+            <Route path="/" exact element={<Dashboard />} />
+          </Route>
         </Routes>
-        <Footer />
       </Router>
     </div>
   );
 }
+
+const WithNav = () => {
+  return (
+    <>
+      <Header />
+      <Menu />
+
+      <Outlet />
+    </>
+  );
+};
+
+const WithoutNav = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
--- src/components/register.component.js
+++ src/components/register.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Loginpage extends Component {
+export default class Registerpage extends Component {
   render() {
     return (
       <body class="hold-transition login-page font-sarabun">
@@ -12,7 +12,7 @@ export default class Loginpage extends Component {
               </a>
             </div>
             <div class="card-body">
-              <p class="login-box-msg">ลงชื่อเข้าใช้</p>
+              <p class="login-box-msg">สมัครสมาชิก</p>
               <form action="../../index3.html" method="post">
                 <div class="input-group mb-3">
                   <input
@@ -38,26 +38,35 @@ export default class Loginpage extends Component {
                     </div>
                   </div>
                 </div>
-                <div class="row">
-                  <div class="col-8">
-                    
+                <div class="input-group mb-3">
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="ยืนยันรหัสผ่าน"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
                   </div>
+                </div>
+                <div class="row">
+                  <div class="col-6"></div>
 
-                  <div class="col-4">
+                  <div class="col-6">
                     <button type="submit" class="btn btn-primary btn-block">
-                      ล็อกอิน
+                      สมัครสมาชิก
                     </button>
                   </div>
                 </div>
               </form>
-              <div class="social-auth-links text-center mt-2 mb-3">
-                
-              </div>
+              <div class="social-auth-links text-center mt-2 mb-3"></div>
 
-              <p class="mb-1">
-                <a href="forgot-password.html">ลืมรหัสผ่าน</a>
+              <p class="mb-0">
+                <a href="register.html" class="text-center">
+                  เข้าสู่ระบบ
+                </a>
               </p>
-              
             </div>
           </div>
         </div>

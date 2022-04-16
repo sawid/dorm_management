2bc1b761d6c586220e8eba1f58e0425b2e4b0eef
Sawid Limkiatsataporn <zasawidza@gmail.com>
2022-04-16 13:41:07 +0700

    update admin manage

--- src/components/function.components/users.js
+++ src/components/function.components/users.js
@@ -10,3 +10,36 @@ export const listUser = async (authtoken) => {
           }
         );
       };
+
+export const changeStatus = async (authtoken, value) => {
+        return await axios.post(
+          process.env.REACT_APP_API + "/change-status", value,
+          {
+            headers: {
+              authtoken,
+            },
+          }
+        );
+      };
+
+export const changeRole = async (authtoken, value) => {
+        return await axios.post(
+          process.env.REACT_APP_API + "/change-role", value,
+          {
+            headers: {
+              authtoken,
+            },
+          }
+        );
+      };
+
+export const removeUser = async (authtoken, id) => {
+        return await axios.delete(
+          process.env.REACT_APP_API + "/users/" + id,
+          {
+            headers: {
+              authtoken,
+            },
+          }
+        );
+      };
--- src/components/manageuser.component.js
+++ src/components/manageuser.component.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import { listUser } from "./function.components/users";
+import { listUser, changeStatus, changeRole, removeUser } from "./function.components/users";
 import { useSelector } from "react-redux";
+import { Switch, Select, Tag } from 'antd';
+import { DeleteFilled } from '@ant-design/icons'
+import moment from "moment/min/moment-with-locales";
 
 const Manageuser = () => {
   const { user } = useSelector((state) => ({...state}))
@@ -21,6 +24,52 @@ const Manageuser = () => {
                 console.log(err);
         })
   };
+
+  const handleonChange = (e, id) => {
+          const value = {
+                  id:id,
+                  enabled:e,
+          };
+          changeStatus(user.token, value)
+          .then(res => {
+                console.log(res)
+                loadData(user.token);
+          })
+          .catch(err => {
+                  console.log(err)
+          })
+  }
+
+  const handleonChangeRole = (e, id) => {
+          const value = {
+                  id:id,
+                  role:e,
+          };
+          changeRole(user.token, value)
+          .then(res => {
+                  console.log(res)
+                  loadData(user.token);
+                })
+          .catch(err => {
+                  console.log(err)
+          })
+  }
+
+  const handleonRemove = (id) => {
+          if(window.confirm("Are you sure Remove")) {
+                  console.log(id)
+                  removeUser(user.token, id)
+                  .then(res => {
+                        console.log(res)
+                        loadData(user.token);
+                      })
+                  .catch(err => {
+                          console.log(err)
+                  })
+          }
+  }
+
+  const roleData = ['admin', 'user']
 
   return (
     <div>
@@ -67,6 +116,7 @@ const Manageuser = () => {
                             <th scope="col">การเปิดใช้</th>
                             <th scope="col">วันที่สร้าง</th>
                             <th scope="col">อัพเดต</th>
+                            <th scope="col">ดำเนินการ</th>
                           </tr>
                         </thead>
                         <tbody>
@@ -76,16 +126,33 @@ const Manageuser = () => {
                               <p>{ item.username }</p>
                             </th>
                             <th scope="row">
-                              <p>{ item.role }</p>
+                              <Select 
+                              style={{ width:"100%" }}
+                              value={ item.role }
+                              onChange={(e) => handleonChangeRole(e, item._id)}
+                              >
+                                      {roleData.map((item, index) => 
+                                      <Select.Option value={ item } key={ index }>
+                                              { item == 'admin' ? <Tag color="red">{ item }</Tag> : <Tag color="green">{ item }</Tag> }
+                                              </Select.Option>
+                                      )}
+                              </Select>
                             </th>
                             <th scope="row">
-                              <p>{ item.enabled }</p>
+                                <Switch checked={ item.enabled } onChange={ (e) => handleonChange(e, item._id) }/>
                             </th>
                             <th scope="row">
-                              <p>{ item.createdAt }</p>
+                              <p>
+                              { moment(item.createdAt).locale('th').format('LLL') }
+                              </p>
                             </th>
                             <th scope="row">
-                              <p>{ item.updatedAt }</p>
+                              <p>
+                                { moment(item.updatedAt).locale('th').startOf(item.updatedAt).fromNow() }
+                                </p>
+                            </th>
+                            <th scope="row">
+                              <p><DeleteFilled onClick={ () => handleonRemove(item._id) }/></p>
                             </th>
                         </tr>
                           )}
